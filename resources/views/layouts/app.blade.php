9486892a1d05059a8b875637a0d9e80a0aa0d5a1
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-07-03 16:56:01 +0500

    no descr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="ru" class="h-100">
 <head>
 
-    <meta name="description" content="Банк тестов ТУИТ."/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
